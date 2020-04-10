8f382b5f6b0a1916c24fca929667076175c4d1a9
shiro <shiro@usagi.io>
2020-04-11 00:03:04 +0200

    Add about me section, update design

--- src/AboutMeSection/AboutMeSection.tsx
+++ src/AboutMeSection/AboutMeSection.tsx
@@ -10,8 +10,21 @@ export interface IAboutMeSection {
 
 const AboutMeSection: React.FC<IAboutMeSection> = (props) => {
     return (
-        <div className={cn()}>
-        </div>
+        <section className={cn(css.section)}>
+            <div className={cn(css.titleContainer)}>
+                <h1 className={cn(css.title)}>About Me</h1>
+                <hr className={cn(css.spacer)}/>
+            </div>
+
+            <p className={cn(css.description)}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis feugiat lorem sit amet mattis gravida. Etiam vel odio libero. Quisque dignissim felis non
+                purus congue pulvinar. Vivamus et arcu ut ex consectetur cursus vitae at erat.
+            </p>
+
+            <p className={cn(css.description)}>
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi tempor leo tellus, auctor tempor nibh aliquam id.
+            </p>
+        </section>
     );
 };
 
