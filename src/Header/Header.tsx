1aa7f515dd8a9c006911b046dc33e47235f86403
shiro <shiro@usagi.io>
2020-04-11 17:11:02 +0200

    Cleanup

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import cn from "classnames";
 
 import css from "./Header.module.scss";
-// import logo from "logo.svg"
 import {ReactComponent as Logo} from "logo.svg"
 
 
@@ -13,8 +12,6 @@ export interface IHeader {
 const Header: React.FC<IHeader> = (props) => {
     return (
         <header className={cn(css.header)}>
-            {/*<img className={cn(css.logo)} src="/resources/logo.svg" alt="logo"/>*/}
-            {/*<img className={cn(css.logo)} src={logo} alt="logo"/>*/}
             <Logo className={cn(css.logo)} />
             <nav className={cn(css.navigation)}>
                 <a className={cn(css.navigationLink)} href="/">Me</a>
