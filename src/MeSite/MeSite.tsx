70bc47f4655f70586472f3e5503aa8d1f28877ac
shiro <shiro@usagi.io>
2020-04-19 13:27:39 +0200

    Add initial author Ld for SEO

--- src/MeSite/MeSite.tsx
+++ src/MeSite/MeSite.tsx
@@ -6,6 +6,33 @@ import ProgrammingSection from "@/ProgrammingSection/ProgrammingSection";
 import DesignSection from "@/DesignSection/DesignSection";
 import ContactSection from "@/ContactSection/ContactSection";
 import Footer from "@/Footer/Footer";
+import {Person} from "schema-dts";import {JsonLd} from "react-schemaorg";
+
+// TODO consolidate all Ld
+const AuthorLd = () => <JsonLd<Person>
+    item={{
+        "@context": "https://schema.org",
+        "@type": "Person",
+        name: "Matic Gačar",
+        birthDate: "1997-6-1",
+        gender: "male",
+        birthPlace: "Ptuj, SI",
+        nationality: "Slovenian",
+        url: "https://usagi.io",
+        alternateName: "白兎 (shirousagi)",
+        colleague: [
+            "https://www.fh-ooe.at/en/"
+        ],
+        knowsLanguage: [
+            "English",
+            "German",
+            "Slovenian",
+        ],
+        email: "shiro@usagi.io",
+        // image: "profile.jpg",
+        jobTitle: "Software Engineer",
+        knowsAbout: ["Software Engineering", "UI/UX Design", "Photography"]
+    }}/>;
 
 
 export interface IMeSite {
@@ -15,6 +42,7 @@ export interface IMeSite {
 const MeSite: React.FC<IMeSite> = (props) => {
     return (
         <div>
+            <AuthorLd />
             <Header/>
             <PortraitSection/>
             <AboutMeSection/>
