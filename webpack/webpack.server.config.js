517d782eee602054b825b0670fc520593d5a317a
shiro <shiro@usagi.io>
2020-04-22 02:00:41 +0200

    Fix regenerator runtime missing in production

--- webpack/webpack.server.config.js
+++ webpack/webpack.server.config.js
@@ -41,6 +41,7 @@ module.exports = {
                         ],
                         plugins: [
                             "babel-plugin-transform-typescript-metadata",
+                            ["@babel/plugin-transform-runtime", { "regenerator": true, }],
                             ["@babel/plugin-proposal-decorators", { "legacy": true }],
                             ["@babel/plugin-proposal-class-properties", { "loose": true }],
                         ],
