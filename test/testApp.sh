911a428fafdac28764a582f8b8e1160541a4b1bb
shiro <shiro@usagi.io>
2020-04-22 22:12:24 +0200

    Update port

--- test/testApp.sh
+++ test/testApp.sh
@@ -2,6 +2,6 @@ docker-compose up -d
 sleep 20
 #docker-compose exec -T php composer req sensiolabs/security-checker
 #docker-compose exec -T php bin/console security:check
-curl http://localhost
-curl -k https://localhost
+curl http://localhost:3000
+curl -k https://localhost:3000
 docker-compose logs
