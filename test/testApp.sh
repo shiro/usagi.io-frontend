71da15491b2f2443e1707e1d417931732d546716
shiro <shiro@usagi.io>
2020-07-04 22:44:50 +0200

    Update test script

--- test/testApp.sh
+++ test/testApp.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-(docker-compose up) &
-sleep 20
+docker-compose up --abort-on-container-exit --exit-code-from e2e
+
+# TODO remove old way of e2e testing
+#(docker-compose up) &
+#sleep 20
 #docker-compose exec -T php composer req sensiolabs/security-checker
 #docker-compose exec -T php bin/console security:check
-curl http://localhost:3000
+#curl http://localhost:3000
 #curl -k https://localhost:3000
+
 docker-compose logs
