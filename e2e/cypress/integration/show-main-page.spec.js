a4eb29ef386a37b01ac653f24dc71874676dcb16
shiro <shiro@usagi.io>
2020-07-04 23:43:01 +0200

    Improve e2e test

--- e2e/cypress/integration/show-main-page.spec.js
+++ e2e/cypress/integration/show-main-page.spec.js
@@ -1,19 +1,21 @@
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
+// returning false here prevents Cypress from failing the test
+Cypress.on("uncaught:exception", (err, runnable) => {
+    // the following error can be safely ignored
+    if(err.message.includes("ResizeObserver loop completed with undelivered notifications."))
+        return false;
+    
+    return true;
 });
 
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    // expect(true).to.equal(true)
-    cy.visit("");
-    
-    // check redirect
-    cy.location('pathname').should('eq', '/me');
-    
-    cy.contains('Matic Gačar');
-    
-    cy.screenshot();
-  })
-})
+describe("Main page", () => {
+    it("Verify the main page loads", () => {
+        cy.visit("");
+        
+        // check redirect
+        cy.location("pathname").should("eq", "/me");
+        
+        cy.contains("Matic Gačar");
+        
+        cy.screenshot();
+    });
+});
